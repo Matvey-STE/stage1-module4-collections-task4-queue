9f44d25b878589db976661e877fdf03a4961e770
Matvey Stetcenko <matveyvs1987@gmail.com>
2023-01-20 18:42:43 +0400

    Problems solved

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,26 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> list = new ArrayDeque<>();
+        list.addLast(Objects.requireNonNull(firstQueue.poll()));
+        list.addLast(Objects.requireNonNull(firstQueue.poll()));
+        list.addLast(Objects.requireNonNull(secondQueue.poll()));
+        list.addLast(Objects.requireNonNull(secondQueue.poll()));
+
+        while (!(firstQueue.isEmpty() || secondQueue.isEmpty())){
+            firstQueue.add(list.pollLast());
+            list.addLast(Objects.requireNonNull(firstQueue.poll()));
+            list.addLast(Objects.requireNonNull(firstQueue.poll()));
+            secondQueue.add(list.pollLast());
+            list.addLast(Objects.requireNonNull(secondQueue.poll()));
+            list.addLast(Objects.requireNonNull(secondQueue.poll()));
+        }
+    return list;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,26 @@
 package com.epam.collections.queue;
 
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> list = new ArrayList<>();
+        int i;
+        for (i = 1; i <= numberOfDishes; i++) {
+            list.add(i);
+        }
+        LinkedList<Integer> queue = new LinkedList<>();
+        int step = everyDishNumberToEat - 1;
+        int j = 0;
+        while (!list.isEmpty()) {
+            j = (j + step) % list.size();
+            int n = list.remove(j);
+            queue.add(n);
+        }
+        return queue;
     }
 }
